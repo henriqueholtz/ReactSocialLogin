e665a490b3d172dad9bbe5baf0aa11f4c888a572
henriqueholtz <henrique_holtz@hotmail.com>
2022-01-24 22:49:19 -0300

    Create NavBar + Home page with sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Navbar } from "./components/Navbar"
+
+import './App.scss'
+import { Home } from "./pages/Home"
+
 function App() {
-  return <h1>ReactSocialLogin</h1>
+  return (
+    <>
+      {/* <h1>ReactSocialLogin</h1> */}
+      <Navbar />
+      <Home />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+
+
+export const Navbar = () => {
+    return (
+        <div className="navbar">
+            <span className="logo">React Social Login (Lama App)</span>
+            <ul className="list">
+                <li className="list-item">
+                    <img src="" alt="" className="avatar" />
+                </li>
+                <li className="list-item">My Name</li>
+                <li className="list-item">Logout</li>
+            </ul>
+        </div>
+    )
+}
